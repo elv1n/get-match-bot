af37a0da0d30d07b6e7f9b1ff6aaa3dfe816c6c0
Elvin <elvin.d@siteplus.com>
2017-11-21 18:23:02 +0200

    Format stream

--- src/api/stream.js
+++ src/api/stream.js
@@ -4,35 +4,34 @@ const fs = require('fs');
 const request = require('request-promise');
 const utils = require('../utils');
 
-
 const URL = 'https://streamable.com/';
 const AUTH = config.get('streamable');
 
 /**
  * Upload from localhost to streamable video hosting
- * @param {string} file name in dist folder 
+ * @param {string} file name in dist folder
  * @returns {string|null} return link to streambable.com or null when failed
  */
 const upload = async file => {
-    // @{link} https://github.com/remixz/to-streamable
-    const Streamable = {
-        method: 'POST',
-        url: `https://api.streamable.com/upload`,
-        formData: { file: fs.createReadStream(utils.getDist(file)) },
-        json: true,
-        auth: AUTH
-    };
+	// @{link} https://github.com/remixz/to-streamable
+	const Streamable = {
+		method: 'POST',
+		url: `https://api.streamable.com/upload`,
+		formData: { file: fs.createReadStream(utils.getDist(file)) },
+		json: true,
+		auth: AUTH
+	};
 
-    try {
-        const {status, shortcode} = await request(Streamable);
-        if (status === 1) {
-            return URL + shortcode;
-        }
-        return null;
-    } catch (e) {
-        console.log(`Cant upload ${file}`);
-        return null;
-    }
+	try {
+		const { status, shortcode } = await request(Streamable);
+		if (status === 1) {
+			return URL + shortcode;
+		}
+		return null;
+	} catch (e) {
+		console.log(`Cant upload ${file}`);
+		return null;
+	}
 };
 
 module.exports = upload;
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,7 +1,7 @@
 function match({ teams, date, uploadUrl }) {
 	return `<b>${teams.home} - ${teams.guest}</b> (${date})
 
-<a href="${uploadUrl}">Open video</a>`;
+<a href="${uploadUrl}">Open in browser</a>`;
 }
 
 module.exports = {
