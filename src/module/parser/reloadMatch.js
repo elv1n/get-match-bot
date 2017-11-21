3dd42c36b60fba261bbe1a5e132ab90bb3c2b369
Elvin <elvin.d@siteplus.com>
2017-11-21 15:29:03 +0200

    Format reloadMatch

--- src/module/parser/reloadMatch.js
+++ src/module/parser/reloadMatch.js
@@ -5,7 +5,9 @@ const getMatch = require('./getMatch');
 const getLinks = require('./getLinks');
 
 module.exports = async _id => {
-	const match = await getMatch({ link: MATCH.HIGHLIGHTS + '/' + _id + '.html' });
+	const match = await getMatch({
+		link: MATCH.HIGHLIGHTS + '/' + _id + '.html'
+	});
 	const matchWithLinks = await getLinks(match);
 	return await db.update(_id, matchWithLinks);
 };
