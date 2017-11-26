b18c8301e543a6a93f6e8e1dcca53d49fc47ec56
Elvin <elvin.d@outlook.com>
2017-11-26 22:03:24 +0200

    Run multiple instance of telegram bot

--- src/api/telegram.js
+++ src/api/telegram.js
@@ -3,44 +3,68 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const messages = require('../utils/messages');
 
-const TOKEN = config.get('telegram.token');
-const CHANNEL = `${config.get('telegram.channel')}`;
+const settings = config.get('telegram');
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+if (!Array.isArray(settings)) {
+	console.error('Telegram config should be an array');
+}
+
+const BOTS = settings.map(({ token, channel }) => ({
+	bot: new TelegramBot(token, { polling: true }),
+	channel
+}));
+
+/**
+ * Run function for multiple bots
+ * @param rule
+ */
+const ruleForBots = rule => (...args) =>
+	BOTS.map(({ bot, channel }) => rule(bot, channel)(...args));
 
 /**
  * Return message when write directly to bot
  */
-bot.on('message', msg =>
-	bot.sendMessage(msg.chat.id, 'Sorry, we dont work directly with users now.')
-);
+
+const acceptMessage = bot => () =>
+	bot.on('message', msg =>
+		bot.sendMessage(
+			msg.chat.id,
+			'Sorry, we dont work directly with users now.'
+		)
+	);
 
 /**
  * Send match details to telegram channel
- * @param {object} doc
+ * @param bot
+ * @param channel
  */
-function sendMatch(doc) {
+const sendMatch = (bot, channel) => doc => {
 	const message = messages.match(doc);
-	return bot.sendMessage(CHANNEL, message, { parse_mode: 'HTML' });
-}
+	return bot.sendMessage(channel, message, { parse_mode: 'HTML' });
+};
 
 /**
  * Edit message when find better video quality
- * @param {object} doc
+ * @param bot
+ * @param channel
  */
-function editMatch(doc) {
+const editMatch = (bot, channel) => doc => {
 	const { message_id } = doc;
 	const message = messages.match(doc);
 	const buttons = messages.getButtons(doc);
 	return bot.editMessageText(message, {
 		message_id,
-		chat_id: CHANNEL,
+		chat_id: channel,
 		parse_mode: 'HTML',
 		reply_markup: buttons
 	});
-}
+};
+/**
+ * Run accept message with multiple bots
+ */
+ruleForBots(acceptMessage)();
 
 module.exports = {
-	sendMatch,
-	editMatch
+	sendMatch: ruleForBots(sendMatch),
+	editMatch: ruleForBots(editMatch)
 };
